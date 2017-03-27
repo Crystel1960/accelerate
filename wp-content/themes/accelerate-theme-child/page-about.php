0ac28d09ee11ea2823cbf4b463af4a78a364a689
Crystel1960 <Crystel1960@gmail.com>
2017-03-27 19:44:33 -0400

    updated about page and style.css

--- wp-content/themes/accelerate-theme-child/page-about.php
+++ wp-content/themes/accelerate-theme-child/page-about.php
@@ -16,11 +16,79 @@
 get_header(); ?>
 
 <section class="home-page">
-	<div class="site-content">
-		<div id="about" role="main">
-			<?php while ( have_posts() ) : the_post(); ?>
-        <div class='homepage-hero'>
-				<?php the_content(); ?>
+	<div class="about"><div id="content">
+		<?php while ( have_posts() ) : the_post(); ?>
+			<div class='homepage-hero'>
+				<h2><?php the_content(); ?></h2>
+			</div>
+		<?php endwhile; // end of the loop. ?>
+	</div><!-- .container -->
+</section><!-- homepage -->
+
+			<?php while ( have_posts() ) : the_post();
+				$our_services = get_field('our_services');
+				$services_content = get_field('services_content');
+				$icon_1 = get_link('icon_1');
+				$title_field_1 = get_field('title_field_1');
+				$description_1 = get_field('description_1');
+				$icon_2 = get_link('icon_2');
+				$title_field_2 = get_field('title_field_2');
+				$description_2 = get_field('description_2');
+				$icon_3 = get_link('icon_3');
+				$title_field_3 = get_field('title_field_3');
+				$description_3 = get_field('description_3');
+				$icon_4 = get_link('icon_4');
+				$title_field_4 = get_field('title_field_4');
+				$description_4 = get_field('description_4');
+				?>
+
+				<div class="top">
+					<h4><?php echo $our_services; ?></h4>
+					<p><?php echo $services_content; ?></p>
+				</div>
+
+				<div class="text-one">
+					<h4><?php echo $title_field_1; ?></h4>
+					<p><?php echo $description_1; ?></p>
+					<?php if($image_1) { ?>
+					</div>
+
+					<div class="i-one">
+						<?php echo wp_get_attachment_image ($icon_1, $size); ?>
+						<?php } ?>
+					</div>
+
+						<div class="text-two">
+							<h4><?php echo $title_field_2; ?></h4>
+							<p><?php echo $description_2; ?></p>
+							<?php if($image_1) { ?>
+							</div>
+							<div class="i-two">
+								<?php echo wp_get_attachment_image ($icon_1, $size); ?>
+								<?php } ?>
+							</div>
+
+								<div class="text-three">
+									<h4><?php echo $title_field_3; ?></h4>
+									<p><?php echo $description_3; ?></p>
+									<?php if($image_1) { ?>
+									</div>
+									<div class="i-three">
+										<?php echo wp_get_attachment_image ($icon_3, $size); ?>
+										<?php } ?>
+									</div>
+
+										<div class="text-four">
+											<h4><?php echo $title_field_4; ?></h4>
+											<p><?php echo $description_4; ?></p>
+											<?php if($image_1) { ?>
+											</div>
+											<div class="i-four">
+												<?php echo wp_get_attachment_image ($icon_4, $size); ?>
+												<?php } ?>
+												<div>
+
+
 			<?php endwhile; // end of the loop. ?>
 		</div><!-- #content -->
 	</div><!-- #primary -->
